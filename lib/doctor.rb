a7e757864133203a25049c34cecfe60af789abd3
Tony Baker <this.tonybaker@gmail.com>
2019-04-11 18:14:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-require 'pry'
->>>>>>> fd806ff7f85be0acd7149b203accb149096b9f18
 class Doctor
   attr_accessor :name
   @@all = []
@@ -21,13 +17,9 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-<<<<<<< HEAD
     new_aptmt = Appointment.new(date, patient, self)
     self.appointments << new_aptmt
     new_aptmt
-=======
-    self.appointments << Appointment.new(date, patient, self)
->>>>>>> fd806ff7f85be0acd7149b203accb149096b9f18
   end
 
   def patients
