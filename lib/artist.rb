d5e1a66ffc36665be464ee7a5f176872c46ee05f
Tony Baker <this.tonybaker@gmail.com>
2019-04-11 18:15:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,13 +13,9 @@ class Artist
   end
 
   def new_song(name, genre)
-<<<<<<< HEAD
     new_song = Song.new(name, self, genre)
     self.songs << new_song
     new_song
-=======
-    self.songs << Song.new(name, self, genre)
->>>>>>> fd806ff7f85be0acd7149b203accb149096b9f18
   end
 
   def songs
